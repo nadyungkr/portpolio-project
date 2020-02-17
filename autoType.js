bc2e2a3ac2308b3885520d8740be3a8402461972
nadyugnkr <dyungkr@naver.com>
2020-02-17 15:07:11 +0900

    반응형 분기 추가, section1 typewriter 반복 및 여러 문장 띄우기 완성

--- autoType.js
+++ autoType.js
@@ -1,39 +1,46 @@
 var headLine = new Array();
 
-headLine[0] = "Web Publisher ";
-headLine[1] = "Front-end Developer ";
-headLine[2] = "Park Hyeon Jeong ";
+headLine[0] = "UI/UX Web Publisher  ";
+headLine[1] = " "; 
+headLine[2] = "Front-end Developer  ";
+headLine[3] = " ";
+headLine[4] = "Hyeon Jeong Park  ";
 var i = 0;
 var j = 0;
+var speed = 200;
 var test = false;
 
 window.onload = function typing(){
 
-    if (i < headLine[j].length+1) {
+    if (i < headLine[j].length + 1) {
         document.getElementById("autoType").innerHTML = headLine[j].substring(0, i);
         i++;
-        setTimeout(typing,200);
+        setTimeout(typing,300);
 
         //headline[j] 다 출력이 되었을때
         if (i == headLine[j].length+1) {
             
             minus_typing();
-            //i=0;
-            //j++;
-            //document.getElementById("autoType").innerHTML = "";
-            //if(j==3) j=0;
+            // i=0;
+            // j++;
+            // document.getElementById("autoType").innerHTML = "";
+            // if(j==3) j=0;
         }
 
     }
 }
-
-function minus_typing(){
+function minus_typing() {
 
     if (i > 0) {
 
         i--;
-        setTimeout(minus_typing,100);
+        setTimeout(minus_typing,50);
 
-        
+        if(i==0){
+            j++;
+                if(j == 5){
+                    j=0;
+                }
+        }
     }
 }
