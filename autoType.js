b0e2dbd77b6cdb65dbd6a94a1ecc77278fb32879
nadyugnkr <dyungkr@naver.com>
2020-02-17 11:11:22 +0900

    main section 1 완성 (첫번째 페이지) 자동 입력 및 지우기 반복 기능 추가

--- autoType.js
+++ autoType.js
@@ -1,11 +1,10 @@
 var headLine = new Array();
 
-headLine[0] = "Web Publisher";
-headLine[1] = "Front-end Developer";
-headLine[2] = "Park Hyeon Jeong";
+headLine[0] = "Web Publisher ";
+headLine[1] = "Front-end Developer ";
+headLine[2] = "Park Hyeon Jeong ";
 var i = 0;
 var j = 0;
-var speed = 100;
 var test = false;
 
 window.onload = function typing(){
@@ -13,16 +12,16 @@ window.onload = function typing(){
     if (i < headLine[j].length+1) {
         document.getElementById("autoType").innerHTML = headLine[j].substring(0, i);
         i++;
-        setTimeout(typing,speed);
+        setTimeout(typing,200);
 
         //headline[j] 다 출력이 되었을때
         if (i == headLine[j].length+1) {
             
             minus_typing();
-            // i=0;
-            // j++;
-            // document.getElementById("autoType").innerHTML = "";
-            // if(j==3) j=0;
+            //i=0;
+            //j++;
+            //document.getElementById("autoType").innerHTML = "";
+            //if(j==3) j=0;
         }
 
     }
@@ -33,7 +32,7 @@ function minus_typing(){
     if (i > 0) {
 
         i--;
-        setTimeout(minus_typing,speed);
+        setTimeout(minus_typing,100);
 
         
     }
